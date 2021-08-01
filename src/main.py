5e5d05785cf616be52f52087b6775706139e3d34
GhastCMD <kraken32bin@protonmail.com>
2021-08-01 12:58:50 -0300

    Started to implement a calendar system

--- src/main.py
+++ src/main.py
@@ -14,31 +14,37 @@ class Employee:
 class PayrollSystem:
     employees = []
     count = 0
-
-    def add_employee(self, name, address, type, attributes):
-        self.employees.append(Employee(name, address, type, attributes, self.count))
-        self.count += 1
+    calendar = [None] * 366
+    current_date = ''
 
     def print_vals(self):
         print('------------ list of employees -------------')
         for employee in self.employees:
             print(employee.__str__())
 
+    def add_employee(self, name, address, type, attributes):
+        self.employees.append(Employee(name, address, type, attributes, self.count))
+        self.count += 1
+
     def del_employee(self, id):
         index = next(i for i, x in enumerate(self.employees) if x.id == id)
         del self.employees[index]
 
+    def launch_timecard(self, id, hours):
+        employee = next(x for x in self.employees if x.id == id)
+
+
 if __name__ == '__main__':
     payroll = PayrollSystem()
 
-    payroll.add_employee('alelo', 'maritona st. 19', 'salaried', 1000)
-    payroll.add_employee('another', 'via st. 11', 'salaried', 1230)
+    payroll.add_employee('alelo', 'maritona st. 19', 'hourly', 12)
+    payroll.add_employee('simple', 'via st. 11', 'salaried', 1230)
     payroll.add_employee('another', 'via st. 12', 'salaried', 1240)
 
     payroll.print_vals()
 
     payroll.del_employee(1)
 
-    print('')
-
     payroll.print_vals()
+
+    payroll.launch_timecard(0, 9)
